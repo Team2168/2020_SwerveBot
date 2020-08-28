696e2c18e334d02b7d69b5a1268f62ebfb884069
Conor Meenan <conorpmeen@grotonk12.org>
2020-08-28 17:57:14 -0400

    Added WheelDrive method and moved wheel init.

--- src/main/java/org/team2168/subsystem/DriveWheel.java
+++ src/main/java/org/team2168/subsystem/DriveWheel.java
@@ -4,18 +4,47 @@ package org.team2168.subsystem;
 
  public class DriveWheel extends Subsystem{
     private TalonFX azimuthTalon;
-    private BaseTalon driveTalon;
-   
+    private TalonFX driveTalon;
+    Wheel wheel = new Wheel(azimuthTalon, driveTalon, 1.0);
     private DriveWheel {
-//Creates new instance of wheel
- Wheel wheel = new Wheel(azimuthTalon, driveTalon, 1.0);
-    
+
     }
-//Allows for the Azimuth and Speed to be changed
+
 public void set(double azimuth, double drive) {
     if (drive == 0){ 
         driver.accept(0d);
         return;
         }
     }
+//Allows for the Azimuth and Speed to be changed
+private WheelDrive getWheels() { 
+TalonFXConfigutation azimuthConfig = new TalonFXConfigutation();
+TalonFXConfigutation.driveConfig = new TalonFXConfigutation();
+azimuthConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.CTRE_MagEncoder_Relative;
+azimuthConfig.continuousCurrentLimit = 10;
+azimuthConfig.peakCurrentDuration = 0;
+azimuthConfig.peakCurrentLimit = 0;
+azimuthConfig.slot0.kp = 10.0;
+azimuthConfig.slot0.kI = 0.0;
+azimuthConfig.slot0.kD = 100.0;
+azimuthConfig.slot0.kF = 0.0;
+azimuthConfig.slot0.integralZone = 0;
+azimuthConfig.slot0.allowableClosedloopError = 0;
+azimuthConfig.motionAcceleration = 10_000;
+azimuthConfig.motionCruiseVelocity = 800;
+driveConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.CTRE_MagEncoder_Relative;
+driveConfig.continuousCurrentLimit = 40;
+driveConfig.peakCurrentDuration = 0;
+driveConfig.peakCurrentLimit = 0;
+    for (int i = 0; i < 4; i++) {
+         TalonFX azimuthTalon = new TalonFX(i);
+         azimuthTalon.configAllSettings(azimuthConfig); 
+         TalonFX drivttalon = new TalonFX(i + 10);
+         driveTalon.configAllSettings(driveConfig);
+
+
+    return;         
+     }
+    }
+
 }
