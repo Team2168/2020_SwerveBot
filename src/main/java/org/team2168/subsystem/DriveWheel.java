f13e961628ad06e0a0e4e6ce5dd397a810012655
James Corcoran <jcorcoran@thayermahan.com>
2020-08-28 20:05:07 -0400

    bring in TalonFX current configs from 2020 drivetrain code

--- src/main/java/org/team2168/subsystem/DriveWheel.java
+++ src/main/java/org/team2168/subsystem/DriveWheel.java
@@ -1,24 +1,31 @@
 package org.team2168.subsystem;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 
 import org.team2168.thirdcoast.swerve.Wheel;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
- public class DriveWheel extends Subsystem{
+public class DriveWheel extends Subsystem {
     private TalonFX azimuthTalon;
     private TalonFX driveTalon;
     Wheel wheel;
+    private final boolean ENABLE_CURRENT_LIMIT = true;
+    private final double CONTINUOUS_CURRENT_LIMIT = 40; //amps
+    private final double TRIGGER_THRESHOLD_LIMIT = 60; //amp
+    private final double TRIGGER_THRESHOLD_TIME = 0.2; //s
 
     private DriveWheel() {
-        TalonFXConfigutation azimuthConfig = new TalonFXConfigutation();
-        TalonFXConfigutation driveConfig = new TalonFXConfigutation();
+        TalonFXConfiguration azimuthConfig = new TalonFXConfiguration();
+        TalonFXConfiguration driveConfig = new TalonFXConfiguration();
+        SupplyCurrentLimitConfiguration talonCurrentLimit;
+
+        talonCurrentLimit = new SupplyCurrentLimitConfiguration(ENABLE_CURRENT_LIMIT,
+        CONTINUOUS_CURRENT_LIMIT, TRIGGER_THRESHOLD_LIMIT, TRIGGER_THRESHOLD_TIME);
 
         azimuthConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.CTRE_MagEncoder_Relative;
-        azimuthConfig.continuousCurrentLimit = 10;
-        azimuthConfig.peakCurrentDuration = 0;
-        azimuthConfig.peakCurrentLimit = 0;
         azimuthConfig.slot0.kP = 10.0;
         azimuthConfig.slot0.kI = 0.0;
         azimuthConfig.slot0.kD = 100.0;
@@ -28,14 +35,14 @@ import edu.wpi.first.wpilibj.command.Subsystem;
         azimuthConfig.motionAcceleration = 10_000;
         azimuthConfig.motionCruiseVelocity = 800;
         driveConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.CTRE_MagEncoder_Relative;
-        driveConfig.continuousCurrentLimit = 40;
-        driveConfig.peakCurrentDuration = 0;
-        driveConfig.peakCurrentLimit = 0;
         // for (int i = 0; i < 4; i++) {
             TalonFX azimuthTalon = new TalonFX(i);
-            azimuthTalon.configAllSettings(azimuthConfig); 
+            azimuthTalon.configAllSettings(azimuthConfig);
+            azimuthTalon.configSupplyCurrentLimit(talonCurrentLimit);
+ 
             TalonFX drivttalon = new TalonFX(i + 10);
             driveTalon.configAllSettings(driveConfig);
+            driveTalon.configSupplyCurrentLimit(talonCurrentLimit);
         // return;         
         // }
 
@@ -43,9 +50,9 @@ import edu.wpi.first.wpilibj.command.Subsystem;
     }
 
     public void set(double azimuth, double drive) {
-    if (drive == 0){ 
-        driver.accept(0d);
-        return;
+        if (drive == 0){ 
+            driver.accept(0d);
+            return;
         }
     }
 
