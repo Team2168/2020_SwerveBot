696dab3eea5da7d3e0870a00a4906425473a9ca2
Cierra <cc362x@gmail.com>
2020-09-13 19:17:06 -0400

    comment out commands for now

--- src/main/java/org/team2168/commands/drivewheel/DriveWithJoystick.java
+++ src/main/java/org/team2168/commands/drivewheel/DriveWithJoystick.java
@@ -35,13 +35,13 @@ public class DriveWithJoystick extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    dw.set(oi.getGunStyleXValue(), Math.abs(oi.getGunStyleYValue())); // abs for testing purposes; set doesn't take negative values
+    //dw.set(oi.getGunStyleXValue(), Math.abs(oi.getGunStyleYValue())); // abs for testing purposes; set doesn't take negative values
   }
 
   // Called once the command ends
   @Override
   public void end() {
-    dw.stop();
+    //dw.stop();
   }
 
   @Override
--- src/main/java/org/team2168/commands/drivewheel/InitializeInternalAzimuthEncoder.java
+++ src/main/java/org/team2168/commands/drivewheel/InitializeInternalAzimuthEncoder.java
@@ -26,7 +26,7 @@ public class InitializeInternalAzimuthEncoder extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    dw.initializeAzimuthPosition();
+    // dw.initializeAzimuthPosition();
   }
 
   // Make this return true when this Command no longer needs to run execute()
