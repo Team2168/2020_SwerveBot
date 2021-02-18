89d998e7add018017565270aee2d7fea0f23b0ac
Cierra <cc362x@gmail.com>
2021-02-17 19:03:11 -0500

    add table of module orientation

--- src/main/java/org/team2168/RobotMap.java
+++ src/main/java/org/team2168/RobotMap.java
@@ -45,6 +45,16 @@ public class RobotMap {
 	/*************************************************************************
 	*                         PDP/CAN DEVICES                                 *
 	*************************************************************************/
+	/** 
+	 * Azimuth 0: 12		Azimuth 1: 2		Azimuth 2: 14		Azimuth 3: 0
+	 * Drive 0: 13			Drive 1: 3			Drive 2: 15			Drive 3: 1
+	 * 
+	 * ORIENTATION OF MODULES:
+	 * 		Front (shooter)
+	 * 				0		1
+	 *				2		3
+	 *		Back (hopper)
+	*/
 	public static final int[] AZIMUTH_TALON_ID = {12, 2, 14, 0};
 	public static final int[] DRIVE_TALON_ID = {13, 3, 15, 1};
 
