43185e6b98b72b314b1b5bc52eb3238f60134dcf
James <jcorcoran@thayermahan.com>
2021-02-25 01:46:06 -0500

    Add PII desvice ID channels per electrical's whiteboard robotmap

--- src/main/java/org/team2168/RobotMap.java
+++ src/main/java/org/team2168/RobotMap.java
@@ -18,11 +18,11 @@ public class RobotMap {
 
 	// Joysticks///////////////////////////////////////////////////////////////
 	public static final int DRIVER_JOYSTICK = 0;
-	
+
 
 
 	// Joystick Control Styles/////////////////////////////////////////////////
-	
+
 
 	// PWM (0 to 9) on RoboRio/////////////////////////////////////////////////
 
@@ -31,7 +31,14 @@ public class RobotMap {
 
 
 	//Analog Input Channels////////////////////////////////////////////////////
-	//Channels 0-3 on Roborio
+  // Channels 0-3 on Roborio
+  public static final int SWERVE_MODULE_0_AI = 0;
+  public static final int SWERVE_MODULE_1_AI = 1;
+  public static final int SWERVE_MODULE_2_AI = 2;
+  public static final int SWERVE_MODULE_3_AI = 3;
+
+  public static final int[] SWERVE_ENCODER_AI = {SWERVE_MODULE_0_AI, SWERVE_MODULE_1_AI,
+                                                 SWERVE_MODULE_2_AI, SWERVE_MODULE_3_AI};
 
 	// Channels 4-7 on MXP
 
@@ -40,27 +47,41 @@ public class RobotMap {
 	*                         Solenoids                                      *
 	*************************************************************************/
 
-	
+
 
 	/*************************************************************************
-	*                         PDP/CAN DEVICES                                 *
-	*************************************************************************/
-	/** 
-	 * Azimuth 0: 12		Azimuth 1: 2		Azimuth 2: 14		Azimuth 3: 0
-	 * Drive 0: 13			Drive 1: 3			Drive 2: 15			Drive 3: 1
-	 * 
-	 * ORIENTATION OF MODULES:
+	*                         PDP/CAN DEVICES                                *
+  *************************************************************************/
+
+  public static final int DRIVE_3_ID = 0;
+  public static final int DRIVE_1_ID = 1;
+  public static final int CLIMBER_1_ID = 2;
+  public static final int CLIMBER_2_ID = 3;
+  public static final int BALANCER_ID = 4;
+  public static final int INTAKE_ID = 5;
+  public static final int AZIMUTH_3_ID = 6;
+  public static final int AZIMUTH_1_ID = 7;
+  public static final int AZIMUTH_2_ID = 8;
+  public static final int AZIMUTH_0_ID = 9;
+  public static final int HOPPER_INDEXER_ID = 10;
+  public static final int SHOOTER_1_ID = 12;
+  public static final int SHOOTER_2_ID = 13;
+  public static final int DRIVE_2_ID = 14;
+  public static final int DRIVE_0_ID = 15;
+  public static final int PIGEON_IMU_ID = 17;
+
+	/**
+	 * ORIENTATION OF SWERVE MODULES:
 	 * 		Front (shooter)
 	 * 				0		1
 	 *				2		3
 	 *		Back (hopper)
 	*/
-	public static final int[] AZIMUTH_TALON_ID = {12, 2, 14, 0};
-	public static final int[] DRIVE_TALON_ID = {13, 3, 15, 1};
+  public static final int[] AZIMUTH_TALON_ID = {AZIMUTH_0_ID, AZIMUTH_1_ID,
+                                                AZIMUTH_2_ID, AZIMUTH_3_ID};
+  public static final int[] DRIVE_TALON_ID   = {DRIVE_0_ID, DRIVE_1_ID,
+                                                DRIVE_2_ID, DRIVE_3_ID};
 
-	// CANifier Azimuth Motor Channels//////////////////////////////////////////
-	public static final RemoteSensorSource[] AZIMUTH_SENSOR_CHANNEL = {RemoteSensorSource.CANifier_PWMInput0, RemoteSensorSource.CANifier_PWMInput1,
-		RemoteSensorSource.CANifier_PWMInput2, RemoteSensorSource.CANifier_PWMInput3};
 
 	// Relay Channels///////////////////////////////////////////////////////////
 
@@ -73,7 +94,7 @@ public class RobotMap {
     /*************************************************************************
 	 *                         SPI DEVICES                                   *
 	 *************************************************************************/
-	
+
 
 
 	/*************************************************************************
