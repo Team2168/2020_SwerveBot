7f286329413f2eb506af03d8f98f0dd057f31ff7
natejank <natejank@protonmail.com>
2021-03-25 10:57:35 -0400

    no more infinite loops
    thanks James

--- src/main/java/org/team2168/commands/limelight/DriveWithLimelight.java
+++ src/main/java/org/team2168/commands/limelight/DriveWithLimelight.java
@@ -12,26 +12,25 @@ import org.team2168.utils.LinearInterpolator;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class DriveWithLimelight extends Command {
-  private Limelight lime;
-  private Drivetrain dt;
-  // private OI oi;
-
-  private LinearInterpolator limeInterpolator;
   private static final double MAX_SPEED = 0.20;
   private static final double DEADZONE = 0.1;
   private static double[][] scaling = {
     {-27.00, -MAX_SPEED},
-    {-0.25, -MAX_SPEED},
+    {-0.50, -MAX_SPEED},
     {-DEADZONE, 0},
     {DEADZONE, 0},
-    {0.25, MAX_SPEED},
+    {0.50, MAX_SPEED},
     {27.00, MAX_SPEED}
   };
+
+  private Limelight lime;
+  private Drivetrain dt;
+  private OI oi;
+  private LinearInterpolator limeInterpolator;
 
   public DriveWithLimelight() {
     lime = Limelight.getInstance();
     dt = Drivetrain.getInstance();
-    // oi = OI.getInstance();
     limeInterpolator = new LinearInterpolator(scaling);
 
     // Use requires() here to declare subsystem dependencies
@@ -42,15 +41,16 @@ public class DriveWithLimelight extends Command {
 
   // Called just before this Command runs the first time
   @Override
-  protected void initialize() {}
+  protected void initialize() {
+    // OI contains a DriveWithLimelight constructor, so if this was in the constructor it creates an infinite loop and never finishes constructing OI
+    oi = OI.getInstance();
+  }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    // dt.drive(oi.getDriverJoystickYValue(), limeInterpolator.interpolate(lime.getXOffset()), 0.0);
-    dt.drive(0.0, limeInterpolator.interpolate(lime.getXOffset()), 0.0);
-
-  }
+    dt.drive(oi.getDriverJoystickYValue(), limeInterpolator.interpolate(lime.getXOffset()), 0.0);
+    }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
