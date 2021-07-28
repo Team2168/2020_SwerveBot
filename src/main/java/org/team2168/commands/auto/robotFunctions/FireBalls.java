8f5777494bff905a6aea3e72b67f456012c30938
Cierra <cc362x@gmail.com>
2021-07-28 17:34:53 -0400

    lowered hopper speed

--- src/main/java/org/team2168/commands/auto/robotFunctions/FireBalls.java
+++ src/main/java/org/team2168/commands/auto/robotFunctions/FireBalls.java
@@ -37,7 +37,7 @@ public class FireBalls extends CommandGroup {
     // a CommandGroup containing them would require both the chassis and the
     // arm.
 
-    addParallel(new DriveHopperWithConstant(1.0));
+    addParallel(new DriveHopperWithConstant(0.7));
     addSequential(new WaitForShooterAtSpeed());
     addSequential(new DriveIndexerWithConstantNoStop(1.0), 0.5);
     addParallel(new DriveIndexerWithConstant(1.0));
