28b47ebff8b896250735cd6e30fca67e85e38ce6
Cierra <cc362x@gmail.com>
2021-07-30 19:31:55 -0400

    extended auto now uses same-file class

--- src/main/java/org/team2168/commands/auto/OppositeTrenchAutoExtendedNoPush.java
+++ src/main/java/org/team2168/commands/auto/OppositeTrenchAutoExtendedNoPush.java
@@ -15,12 +15,15 @@ import org.team2168.commands.flashlight.RunFlashlight;
 import org.team2168.commands.hood_adjust.MoveToFrontTrench;
 import org.team2168.commands.hood_adjust.MoveToWhiteLine;
 import org.team2168.commands.hopper.DriveHopperWithConstant;
+import org.team2168.commands.indexer.DriveUntilBall;
+import org.team2168.commands.indexer.DriveUntilNoBall;
 import org.team2168.commands.intakeMotor.DriveIntakeWithConstant;
 import org.team2168.commands.intakeMotor.IntakeBallStart;
 import org.team2168.commands.intakeMotor.IntakeBallStop;
 import org.team2168.commands.intakePivot.ExtendIntakePneumatic;
 import org.team2168.commands.intakePivot.RetractIntakePneumatic;
 import org.team2168.commands.limelight.DriveWithLimelight;
+import org.team2168.commands.shooter.WaitForShooterAtSpeed;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
@@ -45,13 +48,12 @@ public class OppositeTrenchAutoExtendedNoPush extends CommandGroup {
     addSequential(new DriveWithLimelight(), 5.0);
 
     // stop intake and fire balls
-    //addParallel(new IntakeBallStop());
-    addSequential(new FireBallsAuto(5), 5.0);
-    //addSequential(new FireBalls());
+    addSequential(new FireFiveBalls(), 4.0);
 
     // drive around into rendezvous, turn and extend intake, and drive to pick up balls
     addSequential(new PathCommand("opposite_trench_to_rendezvous", 0.0));
     addSequential(new DriveWithFixedAzimuth(135.0, false));
+    addParallel(new DriveIntakeWithConstant(RobotMap.INTAKE_SPEED));
     addSequential(new ExtendIntakePneumatic());
     addSequential(new PathCommand("opposite_trench_to_rendezvous2", 0.0));
 
@@ -63,5 +65,18 @@ public class OppositeTrenchAutoExtendedNoPush extends CommandGroup {
     
     addSequential(new DriveHopperWithConstant(0.0), 0.1);
     addSequential(new DriveIntakeWithConstant(0.0), 0.0);
+  }
+}
+
+class FireFiveBalls extends CommandGroup {
+  public FireFiveBalls() {
+    addSequential(new WaitForShooterAtSpeed());
+    addParallel(new DriveHopperWithConstant(RobotMap.HOPPER_SPEED));
+    for (int i = 0; i<5; i++) {
+      addSequential(new DriveUntilBall(RobotMap.INDEXER_SPEED));
+      addSequential(new DriveUntilNoBall(RobotMap.INDEXER_SPEED));
+    }
+    addParallel(new DriveHopperWithConstant(0.0), 0.0);
+    addParallel(new DriveIntakeWithConstant(0.0), 0.0);
   }
 }
