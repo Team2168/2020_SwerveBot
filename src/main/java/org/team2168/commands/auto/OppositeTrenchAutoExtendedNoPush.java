9d7e4575180c5b6b1874dd24711576c661b856df
Cierra <cc362x@gmail.com>
2021-07-30 00:02:36 -0400

    started extended opposite trench command

---
+++ src/main/java/org/team2168/commands/auto/OppositeTrenchAutoExtendedNoPush.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.team2168.commands.auto;
+
+import org.team2168.RobotMap;
+import org.team2168.commands.auto.robotFunctions.FireBalls;
+import org.team2168.commands.auto.robotFunctions.FireBallsAuto;
+import org.team2168.commands.flashlight.RunFlashlight;
+import org.team2168.commands.hood_adjust.MoveToFrontTrench;
+import org.team2168.commands.hood_adjust.MoveToWhiteLine;
+import org.team2168.commands.hopper.DriveHopperWithConstant;
+import org.team2168.commands.intakeMotor.DriveIntakeWithConstant;
+import org.team2168.commands.intakeMotor.IntakeBallStart;
+import org.team2168.commands.intakeMotor.IntakeBallStop;
+import org.team2168.commands.intakePivot.ExtendIntakePneumatic;
+import org.team2168.commands.intakePivot.RetractIntakePneumatic;
+import org.team2168.commands.limelight.DriveWithLimelight;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class OppositeTrenchAutoExtendedNoPush extends CommandGroup {
+  /**
+   * Add your docs here.
+   */
+  public OppositeTrenchAutoExtendedNoPush() {
+    // start shooter
+    addParallel(new MoveToFrontTrench());
+
+    // start intake and drive to trench
+    addParallel(new RunFlashlight(1.0));
+    addParallel(new DriveIntakeWithConstant(RobotMap.INTAKE_SPEED));
+    addParallel(new ExtendIntakePneumatic());
+    addSequential(new PathCommand("to_opposite_trench", 0.0));
+    addParallel(new DriveIntakeWithConstant(0.3));
+    addSequential(new RetractIntakePneumatic());
+
+    // drive to shooting location
+    addSequential(new PathCommand("opposite_trench_to_shoot", 20.0));
+    addSequential(new DriveWithLimelight(), 5.0);
+
+    // stop intake and fire balls
+    //addParallel(new IntakeBallStop());
+    addSequential(new FireBallsAuto(5));
+    //addSequential(new FireBalls());
+    addSequential(new DriveHopperWithConstant(0.0), 0.1);
+    addSequential(new DriveIntakeWithConstant(0.0), 0.0);
+  }
+}
