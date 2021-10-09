1c5ff140f3339727615762857f919e295630e507
Shubbii <shubbiievii@gmail.com>
2021-10-08 20:38:18 -0400

    Added a print statement to Drivetrain.getHeading()

--- src/main/java/org/team2168/commands/auto/GoalWallShotReverse.java
+++ src/main/java/org/team2168/commands/auto/GoalWallShotReverse.java
@@ -5,10 +5,11 @@
 package org.team2168.commands.auto;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.team2168.subsystems.Drivetrain;
 
 public class GoalWallShotReverse extends CommandGroup {
-  /** Add your docs here. */
+  Drivetrain drive = Drivetrain.getInstance();
   public GoalWallShotReverse() {
-    addSequential(new PathCommand("goal_wall_shot_reverse", 0));
+    addSequential(new PathCommand("goal_wall_shot_reverse", drive.getHeading()));
   }
 }
