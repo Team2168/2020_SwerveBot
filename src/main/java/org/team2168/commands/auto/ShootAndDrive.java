8231845e5266b0cb474e79e2a7ebf2db33e3d849
sloshyyy <nate@jankowskis.net>
2021-10-15 21:56:33 -0400

    Modified auto to work; updated zeroes
    ShootAndDrive:
      - Fixed issues with imports
      - Added timeouts
    Drivetrain:
      - Updated zeroes

--- src/main/java/org/team2168/commands/auto/ShootAndDrive.java
+++ src/main/java/org/team2168/commands/auto/ShootAndDrive.java
@@ -4,14 +4,17 @@
 
 package org.team2168.commands.auto;
 
+import org.team2168.commands.auto.robotFunctions.FireBallsAuto;
+import org.team2168.commands.hood_adjust.MoveToWhiteLine;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class ShootAndDrive extends CommandGroup {
   /** Add your docs here. */
   public ShootAndDrive() {
-    addParallel(new MoveToWhiteLine());// open up shooter hood and set shooter wheel speed to white line position
+    addParallel(new MoveToWhiteLine(), 5.0);// open up shooter hood and set shooter wheel speed to white line position
     addSequential(new Sleep(), 5.0); // wait five seconds
-    addSequential(new FireBallsAuto(3)); // run the fire balls auto (shoots 3 balls)
+    addSequential(new FireBallsAuto(3), 5.0); // run the fire balls auto (shoots 3 balls)
     addSequential(new PathCommand("shoot_and_drive_path", 0)); // drive 5 feet forward
   }
 }
